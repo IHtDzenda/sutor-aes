9cb5f51661bd64099d68986bfcaad29a3cac304e
“Michal <michalkoudelajunior@seznam.cz>
2025-04-08 14:24:05 +0200

    Implements AESKey encryption.

--- Client.cs
+++ Client.cs
@@ -6,6 +6,23 @@ namespace SutorAes
     byte[] RSA_pub { get; set; }
     byte[] RSA_priv { get; set; }
     Dictionary<Guid, byte[]> AES { get; set; }
+    public static byte[] Encrypt()
+    {
+      if (!AES.ContainsKey(targetId))
+      {
+        throw new ArgumentException("AES klíč pro zadané ID nebyl nalezen.");
+      }
 
+      byte[] aesKey = AES[targetId];
+
+      using (var rsa = RSA.Create())
+      {
+        rsa.ImportRSAPrivateKey(RSA_priv, out _);
+
+        byte[] encryptedData = rsa.Encrypt(aesKey, RSAEncryptionPadding.Pkcs1);
+        return encryptedData;
+      }
+    }
   }
 }
+
