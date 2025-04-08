bf0a68cf7f913d3c79dd44578c96dd2ebe04a00c
MarWer007 <giacomo.giraudo.cz@gmail.com>
2025-04-08 14:16:38 +0200

    feat/decryptMessage

--- Client.cs
+++ Client.cs
@@ -7,5 +7,15 @@ namespace SutorAes
     byte[] RSA_priv { get; set; }
     Dictionary<Guid, byte[]> KnownHosts { get; set; }
 
+    private byte[] DecryptMsg(byte[] msg, Guid id, byte[] IV)
+    {
+      using(var aes = AES.Create())
+      {
+        aes.Key = KnownHosts[id];
+        aes.IV = IV;
+
+        return aes.CreateDecryptor().TransformFinalBlock(msg, 0, msg.Lenght);
+      }
+    }
   }
 }
