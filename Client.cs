c3f5e56399539b5302fe8e8b6dbd559325c5067a
“Michal <michalkoudelajunior@seznam.cz>
2025-04-15 13:42:58 +0200

    Creating encryption, decryption and key generation

--- Client.cs
+++ Client.cs
@@ -1,28 +1,44 @@
+using System.Security.Cryptography;
+using System.Text;
+
 namespace SutorAes
 {
-  class Client
-  {
-    Guid id { get; set; }
-    byte[] RSA_pub { get; set; }
-    byte[] RSA_priv { get; set; }
-    Dictionary<Guid, byte[]> AES { get; set; }
-    public static byte[] Encrypt()
+    class Client
     {
-      if (!AES.ContainsKey(targetId))
-      {
-        throw new ArgumentException("AES klíč pro zadané ID nebyl nalezen.");
-      }
+        Guid id { get; set; }
+        byte[] RSA_pub { get; set; }
+        byte[] RSA_priv { get; set; }
+        Dictionary<Guid, byte[]> KnownHosts { get; set; }
 
-      byte[] aesKey = AES[targetId];
+        public Client()
+        {
+            GenerateRSAKeys();
+        }
 
-      using (var rsa = RSA.Create())
-      {
-        rsa.ImportRSAPrivateKey(RSA_priv, out _);
+        private void GenerateRSAKeys()
+        {
+            using (RSA rsa = RSA.Create(2048))
+            {
+                RSA_pub = rsa.ExportRSAPublicKey();
+                RSA_priv = rsa.ExportRSAPrivateKey();
+            }
+        }
+        public byte[] EncryptRSA(byte[] data)
+        {
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.ImportRSAPublicKey(RSA_pub, out _);
+                return rsa.Encrypt(data, RSAEncryptionPadding.OaepSHA256);
+            }
+        }
 
-        byte[] encryptedData = rsa.Encrypt(aesKey, RSAEncryptionPadding.Pkcs1);
-        return encryptedData;
-      }
+        public byte[] DecryptRSA(byte[] encryptedData)
+        {
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.ImportRSAPrivateKey(RSA_priv, out _);
+                return rsa.Decrypt(encryptedData, RSAEncryptionPadding.OaepSHA256);
+            }
+        }
     }
-  }
 }
-
