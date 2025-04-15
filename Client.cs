8048f14c194fa8612526d595e825d386beeae55f
Frantisek Burkon <frantisek.burkon@proton.me>
2025-04-15 13:08:18 +0200

    add Database class

--- Client.cs
+++ Client.cs
@@ -8,4 +8,24 @@ namespace SutorAes
     Dictionary<Guid, byte[]> AES { get; set; }
 
   }
+
+  class Database
+  {
+    private Dictionary<Guid, byte[]> User2Pubkey { get; set; }
+
+    public Database()
+    {
+      User2Pubkey = new Dictionary<Guid, byte[]>();
+    }
+
+    public void RegisterUser(Guid id, byte[] rsa_pub)
+    {
+      User2Pubkey.Add(id, rsa_pub);
+    }
+
+    public Dictionary<Guid, byte[]> ListUsers()
+    {
+      return User2Pubkey;
+    }
+  }
 }
