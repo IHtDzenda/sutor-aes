e14871cd57dc6b96bec511077a00dc8c49d9b4cc
Kaktus1549 <hummelotup@seznam.cz>
2025-04-22 14:22:55 +0200

    feat: Implement database structure and database models. Also added database function specified on board

--- Database/SQLite.cs
+++ Database/SQLite.cs
@@ -1,4 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+<<<<<<< HEAD
+=======
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+>>>>>>> 6cb4ff4 (feat: Implement database structure and database models. Also added database function specified on board)
 
 public class Database : DbContext
 {
