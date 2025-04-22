cb39301a46f5be68693aea39f513b3fcf24bfa5e
JerrySB03 <gest.2513@gmail.com>
2025-04-22 14:24:35 +0200

    feat: client drafy

--- sutor-aes/Client.cs
+++ sutor-aes/Client.cs
@@ -16,117 +16,142 @@ namespace SutorAes
 
     public Client()
     {
-      GenerateRSAKeys();
-    }
+        Guid id { get; set; }
+        byte[] RsaPub { get; set; }
+        byte[] RsaPriv { get; set; }
+        Dictionary<Guid, byte[]> KnownHosts { get; set; } = new();
+        private event Action<string>? handler;
 
-    private void GenerateRSAKeys()
-    {
-      using (RSA rsa = RSA.Create(2048))
-      {
-        this.RsaPub = rsa.ExportRSAPublicKey();
-        this.RsaPriv = rsa.ExportRSAPrivateKey();
-      }
-    }
+        RSAEncryptionPadding padding = RSAEncryptionPadding.Pkcs1;
 
-    public byte[] EncryptMessage(Guid id, byte[] msg, byte[] IV)
-    {
-      var key = KnownHosts[id];
-      using (Aes aes = Aes.Create())
-      {
-        aes.Key = key;
-        aes.IV = IV;
-        aes.Padding = PaddingMode.PKCS7;
-        aes.Mode = CipherMode.CBC;
-
-        using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+        public Client()
         {
           using (var ms = new MemoryStream())
           {
             using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
             {
-              cs.Write(msg, 0, msg.Length);
-              cs.Close();
+                this.RsaPub = rsa.ExportRSAPublicKey();
+                this.RsaPriv = rsa.ExportRSAPrivateKey();
             }
             return ms.ToArray();
           }
         }
-      }
-    }
 
-    public byte[] DecryptMessage(Guid id, byte[] msg, byte[] IV)
-    {
-      var key = KnownHosts[id];
-      using (Aes aes = Aes.Create())
-      {
-        aes.Key = key;
-        aes.IV = IV;
-        aes.Padding = PaddingMode.PKCS7;
-        aes.Mode = CipherMode.CBC;
-
-        using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
-        {
-          using (var ms = new MemoryStream(msg))
+        public byte[] EncryptMessage(Guid id, byte[] msg, byte[] IV){
+          var key = KnownHosts[id];
+          using (Aes aes = Aes.Create())
           {
-            using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
-            {
-              using (var resultStream = new MemoryStream())
+              aes.Key = key;
+              aes.IV = IV;
+              aes.Padding = PaddingMode.PKCS7;
+              aes.Mode = CipherMode.CBC;
+
+              using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
               {
-                cs.CopyTo(resultStream);
-                return resultStream.ToArray();
+                  using (var ms = new MemoryStream())
+                  {
+                      using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                      {
+                          cs.Write(msg, 0, msg.Length);
+                          cs.Close();
+                      }
+                      return ms.ToArray();
+                  }
               }
-            }
           }
         }
-      }
-    }
 
-    public void OnMessage(Action<string> handler)
-    {
-      this.handler += handler;
-    }
-    public void RecieveMessage(byte[] msg, byte[] IV)
-    {
-      var decryptedMessage = DecryptMessage(id, msg, IV);
-      var message = Encoding.UTF8.GetString(decryptedMessage);
-      handler?.Invoke(message);
-    }
-    public void SendMessage(Client reciever, string msg)
-    {
-      var IV = new byte[16];
-      RandomNumberGenerator.Fill(IV);
-      var encryptedMessage = EncryptMessage(id, Encoding.UTF8.GetBytes(msg), IV);
-      reciever.RecieveMessage(encryptedMessage, IV);
-    }
-
-    public void EstablishConnection(Client other)
-    {
-      byte[] key = new byte[16];
-
-      System.Security.Cryptography.RandomNumberGenerator.Fill(key);
-      other.RecieveConnection(this.id, key);
-      this.KnownHosts.Add(other.id, key);
-    }
-    public void RecieveConnection(Guid id, byte[] key)
-    {
-      this.KnownHosts.Add(id, key);
-    }
-
-    public byte[] EncryptRSA(byte[] data)
-    {
-        using (RSA rsa = RSA.Create())
+        public byte[] DecryptMessage(Guid id, byte[] msg, byte[] IV)
         {
-            rsa.ImportRSAPublicKey(RsaPub, out _);
-            return rsa.Encrypt(data, padding);
+            var key = KnownHosts[id];
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = IV;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.Mode = CipherMode.CBC;
+
+                using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                {
+                    using (var ms = new MemoryStream(msg))
+                    {
+                        using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (var resultStream = new MemoryStream())
+                            {
+                                cs.CopyTo(resultStream);
+                                return resultStream.ToArray();
+                            }
+                        }
+                    }
+                }
+            }
         }
+
+        public void OnMessage(Action<string> handler)
+        {
+            this.handler += handler;
+        }
+        public void RecieveMessage(byte[] msg, byte[] IV)
+        {
+            var decryptedMessage = DecryptMessage(id, msg, IV);
+            var message = Encoding.UTF8.GetString(decryptedMessage);
+            handler?.Invoke(message);
+        }
+        public void SendMessage(Client reciever, string msg)
+        {
+            var IV = new byte[16];
+            var encryptedMessage = EncryptMessage(id, Encoding.UTF8.GetBytes(msg), IV);
+            reciever.RecieveMessage(encryptedMessage, IV);
+        }
+
+        public void EstablishConnection(Client other)
+        {
+          byte[] key =  new byte[16];
+
+         System.Security.Cryptography.RandomNumberGenerator.Fill(key);
+          other.RecieveConnection(this.id, key);
+          this.KnownHosts.Add(other.id, key);
+        }
+        public void RecieveConnection(Guid id, byte[] key)
+        {
+            this.KnownHosts.Add(id, key);
+        }
+        
+        // public byte[] EncryptRSA(byte[] data)
+        // {
+        //     using (RSA rsa = RSA.Create())
+        //     {
+        //         rsa.ImportRSAPublicKey(RsaPub, out _);
+        //         return rsa.Encrypt(data, padding);
+        //     }
+        // }
+        // 
+        // public byte[] DecryptRSA(byte[] encryptedData)
+        // {
+        //     using (RSA rsa = RSA.Create())
+        //     {
+        //         rsa.ImportRSAPrivateKey(RsaPriv, out _);
+        //         return rsa.Decrypt(encryptedData, padding);
+        //     }
+        // }
     }
     
     public byte[] DecryptRSA(byte[] encryptedData)
     {
         using (RSA rsa = RSA.Create())
         {
-            rsa.ImportRSAPrivateKey(RsaPriv, out _);
-            return rsa.Decrypt(encryptedData, padding);
+            User2Pubkey = new Dictionary<Guid, byte[]>();
+        }
+        
+        public void RegisterUser(Guid id, byte[] rsaPub)
+        {
+            User2Pubkey.Add(id, rsaPub);
+        }
+        
+        public Dictionary<Guid, byte[]> ListUsers()
+        {
+            return User2Pubkey;
         }
     }
-  }
 }
