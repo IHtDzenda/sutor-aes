40c8530b09799855fe86a96c5548955e48fd72dd
Patrik Čepek <167709272+CepekPatrik@users.noreply.github.com>
2025-05-13 14:15:43 +0200

    feat: struct message (#17)
    * struct message
    
    * fix: rm unessary usings
    
    ---------
    
    Co-authored-by: Cepek.Pa.2022 <Cepek.Pa.2022@UC301PC12>
    Co-authored-by: IhtDzenda <nev27im@gmail.com>

---
+++ sutor-aes/Message.cs
@@ -0,0 +1,18 @@
+﻿namespace sutor_aes
+{
+  struct Message
+  {
+    public string From { get;set;}
+    public string To { get;set;}
+    public string Content { get;set;}
+    public DateTime Time {  get;set;}
+
+    public Message(string from, string to, string content)
+    {
+      From = from;
+      To = to;
+      Content = content;
+      Time = DateTime.Now;
+    }
+  }
+}
