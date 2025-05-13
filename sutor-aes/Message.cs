0187ba2582e66812731b0a0f4f4446d70aee3a0e
Cepek.Pa.2022 <Cepek.Pa.2022@UC301PC12>
2025-05-13 14:03:06 +0200

    struct message

---
+++ sutor-aes/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace sutor_aes
+{
+    struct Message
+    {
+        public string From { get;set;}
+        public string To { get;set;}
+        public string Content { get;set;}
+        public DateTime Time {  get;set;}
+
+        public Message(string from, string to, string content)
+        {
+            From = from;
+            To = to;
+            Content = content;
+            Time = DateTime.Now;
+        }
+    }
+}
