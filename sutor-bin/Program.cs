ad573fff111ee6e8417eb7a2441d63cd89f556df
Frantisek Burkon <frantisek.burkon@proton.me>
2025-06-03 14:17:06 +0200

    added sutor-bin

---
+++ sutor-bin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+using SutorAes;
+
+public class Program
+{
+    public static void Main()
+    {
+        Client client = new Client("user");
+        Console.WriteLine(Convert.ToBase64String(client.RsaPub));
+    }
+}
