0c64c8aa9c1bca05b0c071129425d18b4b84f334
Jan <111608778+IHtDzenda@users.noreply.github.com>
2025-06-03 14:10:39 +0200

    test: fix and add client (#18)
    Co-authored-by: JerrySB03 <gest.2513@gmail.com>

--- sutor-aes/Client.cs
+++ sutor-aes/Client.cs
@@ -4,7 +4,7 @@ using System;
 
 namespace SutorAes
 {
-    class Client
+    public class Client
     {
         Guid Id { get; set; }
         public string Name { get; set; }
---
+++ tests/ClientTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+using SutorAes;
+
+namespace tests;
+
+public class ProgramTests
+{
+    private byte[] _key;
+    private byte[] _iv;
+
+    [SetUp]
+    public void Setup()
+    {
+        _key = new byte[32];
+        Random random = new Random();
+        random.NextBytes(_key);
+
+        _iv = new byte[16];
+        random.NextBytes(_iv);
+    }
+
+    [Test]
+    public void EstablishConnection_SendMessage()
+    {
+        string originalText = "This is a test message for encryption and decryption";
+
+        Client bob = new Client("Bob");
+        Client alice = new Client("Alice");
+
+        bob.EstablishConnection(alice);
+
+        bool recieved = false;
+        alice.OnMessage((string msg) => recieved = msg == originalText);
+        bob.SendMessage(alice, originalText);
+        Assert.That(recieved);
+    }
+}
